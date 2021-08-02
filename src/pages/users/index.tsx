d4b9f8d517acb7be3f810289be174ecc3171a598
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-08-02 16:31:54 -0300

    Componentização dos da secção principal

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,39 +1,27 @@
-import { Box, Button, Flex, Heading, Icon} from "@chakra-ui/react";
+import { Button, Flex, Icon} from "@chakra-ui/react";
 import { RiAddLine } from "react-icons/ri";
-import { Header } from "../../components/Header";
+import { MainSection } from "../../components/MainSection";
 import { Pagination } from "../../components/Pagination";
-import { Sidebar } from "../../components/Sidebar";
+import { SectionHeading } from "../../components/SectionHeading";
 import { UsersList } from "../../components/UsersList";
 
 export default function UserList() {
   return (
-    <Box>
-      <Header />
-      <Flex
-        w="100%"
-        maxWidth={1480}
-        my="6"
-        mx="auto"
-        px="6"
-      >
-        <Sidebar/>
-        <Box flex="1" borderRadius={8} bgColor="gray.800" p="8">
-          <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
-            <Button 
-              as="a" 
-              size="sm" 
-              fontSize="small" 
-              colorScheme="pink" 
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar Usuário
-            </Button>
-          </Flex>
-          <UsersList />
-          <Pagination />
-        </Box>
-      </Flex> 
-    </Box>
+    <MainSection>
+      <Flex mb="8" justify="space-between" align="center">
+        <SectionHeading title="Usuários"/>
+        <Button 
+          as="a" 
+          size="sm" 
+          fontSize="small" 
+          colorScheme="pink" 
+          leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+        >
+          Criar Usuário
+        </Button>
+      </Flex>
+      <UsersList />
+      <Pagination />
+    </MainSection>
   );
 }
