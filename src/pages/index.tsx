4d557e5d982c8dd423499e8747be8cf979554ec3
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-08-02 14:01:50 -0300

    Estilização da página de SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+
+export default function SignIn() {
   return (
-    <div>
-      <h1>DashGo</h1>
-    </div>
-  )
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8" // Nem 8px nem 8rem, mas sim 2rem * 4
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email"/>
+          <Input name="password" label="Senha" type="password"/>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
